2c99d9b1a0d4269223f6d776edf09784b74a86ba
ayaka-ii <ayaka.1234.k@gmail.com>
2025-01-19 14:35:10 +0900

    WEBAPI作成（Circle）

--- code/django/UserManagementSystem/UserManagement/api/urls.py
+++ code/django/UserManagementSystem/UserManagement/api/urls.py
@@ -3,7 +3,9 @@ from UserManagement.api import views
 
 urlpatterns = [
     path('users/', views.UserView.as_view(), name='list'),
-    path('users/<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('users/<int:pk>/', views.UserDetailView.as_view(), name='detail'),
+    path('circles/', views.CircleView.as_view(), name='list'),
+    path('circles/<int:pk>/', views.CircleDetailView.as_view(), name='detail'),
 ]
 # < >はプレースホルダー。pk＝プライマリーキー。djangoはデフォでidがpkとして作成されるのでidの値が渡される
 # importしているviewsの関数指定（この後作成）
--- code/django/UserManagementSystem/UserManagement/api/views.py
+++ code/django/UserManagementSystem/UserManagement/api/views.py
@@ -2,16 +2,27 @@ from rest_framework import generics
 from UserManagement.models import User, Circle
 from UserManagement.api.serializers import UserSerializer, CircleSerializer
 
+# generics.ListCreateAPIViewクラスの継承によってリストを簡単に作成できる
+# Listはデータを全部取得、Retrieveはデータを１レコード取得
+
+
+# 以下Userテーブル用
 class UserView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     # ▼order_byの書き方。-カラムで降順
     # queryset = User.objects.all().order_by('-id')
     serializer_class = UserSerializer
     
-class DetailView(generics.RetrieveUpdateDestroyAPIView):
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-# generics.ListCreateAPIViewクラスの継承によってリストを簡単に作成できる
-# Listはデータを全部取得、Retrieveはデータを１レコード取得
 
+# 以下Circleテーブル用
+class CircleView(generics.ListCreateAPIView):
+    queryset = Circle.objects.all()
+    serializer_class = CircleSerializer
+    
+class CircleDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Circle.objects.all()
+    serializer_class = CircleSerializer
