3d3032a9ecdacadf3a5699a26f1c1ad003dfaf06
ayaka-ii <ayaka.1234.k@gmail.com>
2025-02-07 22:29:56 +0900

    vuejs再々構築

---
+++ code/vue/UMS/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+
+<template>
+  <!-- vuetifyのコンポーネントはv-appで囲う -->
+  <v-app>
+    <header>
+      <v-app-bar app theme="dark">
+        <v-toolbar-title>Tomoeda Academy</v-toolbar-title>
+      </v-app-bar>
+    </header>
+
+    <v-main>
+      <!-- メイン表示　ルーティングページ -->
+      <router-view />
+    </v-main>
+
+  </v-app>
+
+</template>
+
+<style>
+
+</style>
